9c64249e22a5c7539d709c0855e1f1b8f11f1fba
Daniel <dan@dragomirweb.com>
2018-04-28 10:11:25 +0300

    add auth prep

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,13 @@
 
 
-var express = require("express");
-let plivo = require('plivo');
-var calls = require("../models/calls");
-//var todos = require("../../mock/todos.json");
+const express = require("express");
+const plivo = require('plivo');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const calls = require("../models/calls");
+const User  = require("../models/user.model");
 const query = calls.find();
 
 
@@ -419,6 +423,68 @@ router.get('/answer', function(req, res) {
 });
 
 
+// router.post('/signup', function(req, res) {
+//     bcrypt.hash(req.body.password, 10, function(err, hash){
+//        if(err) {
+//           return res.status(500).json({
+//              error: err
+//           });
+//        }
+//        else {
+//           const user = new User({
+//              _id: new  mongoose.Types.ObjectId(),
+//              email: req.body.email,
+//              password: hash    
+//           });
+//           user.save().then(function(result) {
+//              console.log(result);
+//              res.status(200).json({
+//                 success: 'New user has been created'
+//              });
+//           }).catch(error => {
+//              res.status(500).json({
+//                 error: err
+//              });
+//           });
+//        }
+//     });
+//  });
+
+ router.post('/signin', function(req, res){
+    User.findOne({email: req.body.email})
+    .exec()
+    .then(function(user) {
+       bcrypt.compare(req.body.password, user.password, function(err, result){
+          if(err) {
+             return res.status(401).json({
+                failed: 'Unauthorized Access'
+             });
+          }
+          if(result) {
+            const JWTToken = jwt.sign({
+                email: user.email,
+                _id: user._id
+              },
+              'callapp',
+               {
+                 expiresIn: '2h'
+               });
+               return res.status(200).json({
+                success: 'Welcome to the JWT Auth',
+                token: JWTToken
+              });
+          }
+          return res.status(401).json({
+             failed: 'Unauthorized Access'
+          });
+       });
+    })
+    .catch(error => {
+       res.status(500).json({
+          error: error
+       });
+    });;
+ });
 
 module.exports = router;
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 'use strict';
 var express = require("express");
 var dotenv = require("dotenv").config();
+var path = require("path");
 var router = require("./api/index");
+var jwt = require('express-jwt');
+var unless = require('express-unless');
 var bodyParser = require('body-parser');
 var socket_io  = require('socket.io');
 var app = express();
@@ -23,9 +26,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', router);
-//static for DigitalOcean should be /root/public
-app.use('/', express.static('/root/public'));
 
+app.use(jwt({ secret: 'callapp',credentialsRequired: true}).unless({path: ['/api']}));
+app.use(function (err, req, res, next) {
+    console.log(req)
+    if (err.name === 'UnauthorizedError') {
+        if(req.path !== '/signin'){
+            res.redirect('/signin');
+            res.send(req);
+        } else {
+            res.status(401).sendFile(path.join(__dirname + '/views/login.html'));
+        }
+    }
+  });
+
+//static for DigitalOcean should be /root/public
+app.use('/', express.static('public'));
 
 
 http.listen(port, function() {
